5881c4970ba4e45b038234284a464207cf6a4365
akdlakdrn <46807135+akdlakdrn@users.noreply.github.com>
2019-05-18 03:48:05 +0900

    Applicant Update

--- src/test/java/com/cos/IotProject/model/Applicant.java
+++ src/test/java/com/cos/IotProject/model/Applicant.java
@@ -1,5 +1,16 @@
 package com.cos.IotProject.model;
 
 public class Applicant {
-
+  
+  private int num;
+	
+	private String applicantID;
+	private String applicantPassword;
+	private String applicantName;
+	private String applicantEmail;
+	private String applicantAddr;
+	private String applicantPhone;
+	private String applicantRRN ; //주민등록번호
+	private String applicantGender;
+	private int applicantAge;
 }
